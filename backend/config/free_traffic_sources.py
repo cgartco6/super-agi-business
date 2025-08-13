bbd2d4367b697aeb5c775cf093ff7966b922d557
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2025-08-13 18:44:26 +0200

    Create free_traffic_sources.py

---
+++ backend/config/free_traffic_sources.py
@@ -0,0 +1,53 @@
+SA_FREE_TRAFFIC_SOURCES = {
+    'social_media': {
+        'facebook': [
+            'South African Entrepreneurs Group',
+            'Cape Town Small Business Network',
+            'Gauteng Business Owners'
+        ],
+        'linkedin': [
+            'SA Tech Startups group',
+            'Digital Marketing South Africa'
+        ],
+        'reddit': [
+            'r/southafrica',
+            'r/capetown'
+        ]
+    },
+    'forums': [
+        {
+            'name': 'HelloPeter Discussions',
+            'url': 'https://www.hellopeter.com/discussions',
+            'topics': ['business', 'technology']
+        },
+        {
+            'name': 'MyBroadband SME Forum',
+            'url': 'https://mybroadband.co.za/forum/forums/small-medium-business.42/',
+            'topics': ['tech', 'automation']
+        }
+    ],
+    'classifieds': [
+        {
+            'name': 'Gumtree',
+            'url': 'https://www.gumtree.co.za',
+            'categories': ['business', 'services']
+        },
+        {
+            'name': 'OLX',
+            'url': 'https://www.olx.co.za',
+            'categories': ['business-equipment', 'services']
+        }
+    ],
+    'directories': [
+        {
+            'name': 'SA Startup Directory',
+            'url': 'https://www.sastartupdirectory.co.za',
+            'listing_type': 'free'
+        },
+        {
+            'name': 'Local Business Listings',
+            'url': 'https://www.local.co.za',
+            'listing_type': 'free basic'
+        }
+    ]
+}
